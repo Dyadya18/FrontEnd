1c264d086b76bf357ca4ed7f163e09575415cb34
Vladyslav <vladoceretnuy@gmail.com>
2021-03-10 11:32:33 +0200

    Initial commit

---
+++ Shop/js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+    $(".banner-section_slider").slick({
+        dots:true,        
+        prevArrow: '<button class="banner-section_slider-btn banner-section_slider-btnprev"><img class="img_arrow" src="img/54321.svg" alt=""></button>',
+        nextArrow: '<button class="banner-section_slider-btn banner-section_slider-btnnext"><img class="img_arrow" src="img/32213.svg" alt=""></button>'
+    })
+})
